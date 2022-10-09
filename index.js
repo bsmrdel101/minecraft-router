149cfbd00c10885a106c07b5ca2f05a43db5a516
Bennett Smrdel <fireknights318@gmail.com>
2022-10-09 09:48:20 -0500

    feat: Added discord bot
    - Bot can connect.
    - Created discord bot commands.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { Client, GatewayIntentBits, channelLink, GuildChannel, createChannel } = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 app.use(express.json({ extended: false }));
 
@@ -9,12 +12,29 @@ const test = require('./api/test');
 
 app.use('/api/test', test);
 
-// app.get('/', (req, res) => {
-//     console.log('Yay it worked');
-//     res.json({ text: 'Hello World!' });
-// })
+
+// Discord bot
+client.once('ready', () => {
+	console.log('Bot is ready Ready!');
+    console.log('===================');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.login(token);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
-
